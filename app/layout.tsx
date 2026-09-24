6dc11ba34afe5f7369847a291d4a6079fc731c0c
TeepeeATM <dgh7d2j6p2@privaterelay.appleid.com>
2026-09-24 13:58:15 -0500

    Load hero carousel styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import "./social.css";
+import "./hero-carousel.css";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 import { ScrollReveal } from "@/components/ScrollReveal";
